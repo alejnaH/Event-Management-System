49ef42b50d7016214d6bef8fabf3332ebf2a72f9
Alejna Hasanagic <alejna.igrice@gmail.com>
2024-03-29 22:36:49 +0100

    Fixed up some bugs, added contents to the json file, still doesnt work but working on it

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,5 +18,61 @@ function closeModal() {
 
 
 
+//dashboard
+document.addEventListener("DOMContentLoaded", function(event) {
+    // Note: No need to manually handle modal closing since Bootstrap handles it
 
-// Note: Bootstrap handles opening the modal via data-bs-toggle and data-bs-target attributes
+    // Add the event listener for the "Learn More" button here
+    var learnMoreButton = document.querySelector('.learn-more');
+    if (learnMoreButton) {
+        learnMoreButton.addEventListener('click', function(event) {
+            event.preventDefault();
+            var scrollSection = document.getElementById('scroll');
+            if (scrollSection) {
+                scrollSection.scrollIntoView({behavior: 'smooth'});
+            }
+        });
+    }
+});
+//dashboard 
+
+
+
+
+
+
+
+//Bootstrap handles opening the modal via data-bs-toggle and data-bs-target attributes
+
+//jsonData
+
+$(document).ready(function() {
+    // Fetch the JSON data
+    fetch('views/data.json')
+      .then(response => response.json())
+      .then(data => {
+        // For each item in the data, populate the block
+        $.each(data, function(i, item) {
+          // Select the block and set its content
+          var block = $('.col.mb-5').eq(i);
+          block.find('.date p').text(item.date);
+          block.find('.text-center h5').text(item.eventName);
+  
+          // Attach a click event handler to the block
+          block.on('click', function() {
+            // Select the modal and set its content
+            var modal = $('#myModal');
+            modal.find('.modal-title').text(item.eventName);
+            modal.find('.modal-body').html(
+              '<p>Date: ' + item.date + '</p>' +
+              '<p>Time: ' + item.time + '</p>' +
+              '<p>Country: ' + item.country + '</p>' +
+              '<p>City: ' + item.city + '</p>' +
+              '<p>Address: ' + item.address + '</p>' +
+              '<p>Description: ' + item.description + '</p>'
+            );
+          });
+        });
+      })
+      .catch(error => console.error('Error:', error));
+  });
