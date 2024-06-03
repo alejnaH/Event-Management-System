9291ac400330d630ffc3e455f526e62db0d7f407
Alejna Hasanagic <alejna.igrice@gmail.com>
2024-06-03 17:58:15 +0200

    Setting up  index.php

---
+++ live-backend/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'rest/routes/middleware_routes.php';
+require 'rest/routes/event_routes.php';
+require 'rest/routes/auth_routes.php';
+
+Flight::start();
